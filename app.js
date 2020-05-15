dba995fe7577ea0e07681dd679b297d5b2193847
Thiago Ramalho <thiago.nramalho@gmail.com>
2020-05-14 22:49:24 -0300

    update jade to pug

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'pug');
 
 // Passport middleware
 app.use(passport.initialize());
